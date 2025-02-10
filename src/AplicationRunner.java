c2bd1500e8101412407f1e826d141a022cd4a66c
fazelim3 <fazeli.workbox@gmail.com>
2025-02-10 23:37:54 +0330

    use 3-tire architecture and finish part5

--- src/AplicationRunner.java
+++ src/AplicationRunner.java
@@ -2,7 +2,11 @@ import com.mysite.customer.view.ConsoleUI;
 
 public class AplicationRunner {
     public static void main(String[] args) {
-        ConsoleUI consoleUI=new ConsoleUI();
-        consoleUI.startMenu();
+
+
+            ConsoleUI consoleUI = new ConsoleUI();
+            consoleUI.startMenu();
+
+
     }
 }
